8b95b08cba5a4e87a044c44b91f8ebd9c24a27fd
Marlon Baltodano <marlonbaltodano11@gmail.com>
2024-12-12 16:31:36 -0600

    feat(backend): update allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'django-insecure-^(om5j8&y_=(86bof!*3+ekx77j6d(2bg*3q=lmwyi^&c8csq@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'syllabus.hqcoders.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
